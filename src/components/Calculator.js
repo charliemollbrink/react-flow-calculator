47bcb5df110df4a7d66776d61f3693b886544b53
Charlie Möllbrink <charlie.mollbrink@gmail.com>
2018-04-20 20:54:05 +0200

    Flowify Button and Calculator Components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,20 +2,28 @@
 import React, { Component } from 'react';
 import Button from './Button';
 
-class Calculator extends Component {
+type State = {
+    values: string,
+    operators: string[]
+};
 
-    constructor(props){
-        super(props);
-        this.state = {
-            values: '',
-        }
+class Calculator extends Component<{}, State> {
+    state = {
+        values: '',
+        operators: ['/', '-', '+', 'x', '%'],
     }
 
-    renderButton(value: string|number, name = value) {
+    renderButton(value: string, name: string = value) {
         return <Button
             key={value}
             value={name}
             onClick={() => this.onClick(value)}/>
+    }
+    renderNumberButton(value: string, name: string = value) {
+        return <Button
+            key={value}
+            value={name}
+            onClick={() => this.onNumberClick(value)}/>
     }
 
     renderFuncButton(value: string, fn: Function) {
@@ -26,9 +34,10 @@ class Calculator extends Component {
     }
 
     render() {
-        const displayString = this.state.values.split('').map((val) => {
-            const a = (val === '/')?'\u00F7':val; return a;
-        });
+        const displayString = (this.state !== undefined) ?
+            this.state.values.split('').map((val) => {
+                const a = (val === '/')?'\u00F7':val; return a
+            }) : '';
 
         return (
             <div className='calculator'>
@@ -42,35 +51,45 @@ class Calculator extends Component {
                     {this.renderFuncButton('back', this.goBack.bind(this))}
                 </div>
                 <div>
-                    {this.renderButton(7)}
-                    {this.renderButton(8)}
-                    {this.renderButton(9)}
+                    {this.renderNumberButton('7')}
+                    {this.renderNumberButton('8')}
+                    {this.renderNumberButton('9')}
                     {this.renderButton('+')}
                 </div>
                 <div>
-                    {this.renderButton(4)}
-                    {this.renderButton(5)}
-                    {this.renderButton(6)}
+                    {this.renderNumberButton('4')}
+                    {this.renderNumberButton('5')}
+                    {this.renderNumberButton('6')}
                     {this.renderButton('x')}
                 </div>
                 <div>
-                    {this.renderButton(1)}
-                    {this.renderButton(2)}
-                    {this.renderButton(3)}
+                    {this.renderNumberButton('1')}
+                    {this.renderNumberButton('2')}
+                    {this.renderNumberButton('3')}
                     {this.renderFuncButton('%', this.sum.bind(this))}
                 </div>
                 <div>
-                    {this.renderButton('0')}
-                    {this.renderButton('.')}
+                    {this.renderNumberButton('0')}
+                    {this.renderNumberButton('.')}
                     {this.renderFuncButton('=', this.sum.bind(this))}
                 </div>
             </div>
         );
     }
 
-    onClick(i) {
-        if (isNaN(this.state.values[this.state.values.length -1]) && isNaN(i)) return;
-        this.setState({values: this.state.values.concat(i)});
+    onClick(i: string) {
+        const prevI = this.state.values[this.state.values.length -1];
+        if (prevI === i){
+            return;
+        } else if (this.state.operators.indexOf(prevI) > -1) {
+            this.setState({ values: this.state.values.slice(0, -1).concat(i) });
+        } else {
+            this.setState({ values: this.state.values.concat(i) });
+        }
+    }
+
+    onNumberClick(i: string) {
+        this.setState({ values: this.state.values.concat(i) });
     }
 
     clear() {
